fedbe82bdd346b01fa6a4e5c84a7f2f00b650b5c
Frank Oxener <frank.oxener@gmail.com>
2009-08-20 12:30:11 +0200

    Initial commit

---
+++ test/test_helper.rb
@@ -0,0 +1,9 @@
+begin
+  require 'shoulda'
+rescue LoadError
+  require 'rubygems' unless ENV['NO_RUBYGEMS']
+  require 'shoulda'
+end
+
+$:.unshift(File.dirname(__FILE__) + '/../lib')
+require 'With_properties'
